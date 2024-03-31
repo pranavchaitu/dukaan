95ed863003df1c6d9ac46b47bf1256f27be3f29b
pranavchaitu <pranavchaitu676@gmail.com>
2024-03-31 19:32:40 +0530

    final version

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,7 +2,7 @@ import { Input } from "./InputBox";
 
 export function TopBar() {
     return <div className="flex justify-between p-3 gap-6 items-center h-12 bg-white border border-gray-200 fixed z-20 right-0 left-0 md:left-44">
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6 block md:hidden">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
@@ -12,6 +12,9 @@ export function TopBar() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-8 -5 37 35" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
             </svg>
+            <div className="hidden md:block text-xs text-gray-700">
+                How it works
+            </div>
         </div>
         <div>   
             <Input 
--- src/components/sidebar/BottomSection.jsx
+++ src/components/sidebar/BottomSection.jsx
@@ -1,25 +1,22 @@
-export const TopSection = () => {
+export const BottomSection = () => {
     return <>
-        <div className="flex justify-between p-3 items-center">
-            <div className="flex m-2">
-                <div className="bg-gray-300 rounded">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
-                    </svg>
+        <div className="flex p-3 items-center justify-center">
+            <div className=" bg-gray-600 bg-opacity-30 rounded">
+                <div className="flex m-2">
+                    <div className=" bg-gray-700 bg-opacity-50 rounded flex items-center px-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
+                        </svg>
+                    </div>
+                    <div className="ml-2">
+                        <p className="text-xs text-gray-300">
+                            Available Credits
+                        </p> 
+                        <p className="text-gray-50 text-sm">
+                            224.10
+                        </p>
+                    </div>
                 </div>
-                <div className="h-4 ml-3">
-                    <p className="text-sm">
-                        Dukaan
-                    </p> 
-                    <p className="underline text-gray-400 text-xs">
-                        Visit store
-                    </p>
-                </div>
-            </div>
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                    <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                </svg>
             </div>
         </div>
     </>
